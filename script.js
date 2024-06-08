3d9b3d2b0f1a68a01392cdaacc2989564d973c16
Glen Perera <123937629+jumangl@users.noreply.github.com>
2024-06-08 18:59:14 +0530

    Done with all parts

--- script.js
+++ script.js
@@ -6,3 +6,36 @@ document.addEventListener("DOMContentLoaded", function () {
     navbar.classList.toggle("show");
   });
 });
+
+// Smooth scrolling function
+function smoothScroll(target, duration) {
+  var targetId = target.startsWith("#") ? target.slice(1) : target; // Remove '#' if present
+  var targetElement = document.getElementById(targetId);
+  if (!targetElement) return; // Exit if target element not found
+  var targetPosition = targetElement.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var scrollAmount = easeInOutQuad(
+      timeElapsed,
+      startPosition,
+      distance,
+      duration
+    );
+    window.scrollTo(0, scrollAmount);
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  function easeInOutQuad(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
