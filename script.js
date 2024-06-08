f2f5493e755cb55d7a29d1a65d031ddcfdb3a12c
Glen Perera <123937629+jumangl@users.noreply.github.com>
2024-06-08 17:16:01 +0530

    Done with the main parts

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const navbar = document.querySelector(".navbar");
+
+  menuIcon.addEventListener("click", function () {
+    navbar.classList.toggle("show");
+  });
+});
